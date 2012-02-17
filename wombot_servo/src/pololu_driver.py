dfd3e4b352b1533be125880153a2a20956f97f6a
tscott <tim@scott.id.au>
2012-02-18 00:25:55 +1100

    Adjusted polulo_driver

--- wombot_servo/src/pololu_driver.py
+++ wombot_servo/src/pololu_driver.py
@@ -5,6 +5,8 @@ import serial
 
 from art_msgs.msg import SteeringCommand
 from art_msgs.msg import SteeringState
+from art_msgs.msg import ThrottleCommand
+from art_msgs.msg import ThrottleState
 
 
 class PololuDriver(object):
@@ -12,14 +14,27 @@ class PololuDriver(object):
         rospy.init_node('pololu_driver')
         rospy.on_shutdown(self.ShutdownCallback)
         rospy.Subscriber("steering/cmd",SteeringCommand, self.SteeringCallback)
+        rospy.Subscriber("throttle/cmd",ThrottleCommand, self.ThrottleCallback)
         
         port = '/dev/ttyACM0'
+        
+        self._steering_servo_1 = 0
+        self._steering_servo_2 = 1
+        self._throttle_servo   = 2      # Neutral = 1340
+        self._gearchange_servo = 3      # High gear = 2000,  Low gear = 1000
+        self._difflock_servo_1 = 4      # Locked = 1000, Unlocked = 2000
+        self._difflock_Servo_2 = 5      # Locked = 1000, Unlocked = 2000
         
         try:
             self.ser = serial.Serial(port)
             self.ser.open()
             self.ser.write(chr(0xAA))
-            self.ser.flush()            
+            self.ser.flush()
+            
+            self.ser.write(self.get_command(self._steering_servo_1,1500))
+            self.ser.write(self.get_command(self._steering_servo_2,1500))
+            self.ser.write(self.get_command(self._throttle_servo,1340))
+            
         except serial.serialutil.SerialException as e:
             rospy.logerr(rospy.get_name()+": Error opening or initialising port "+port)
             exit(1)
@@ -40,22 +55,27 @@ class PololuDriver(object):
         return serialBytes
     
     def ShutdownCallback(self):
-    rospy.loginfo("Shutting down")
+        rospy.loginfo("Shutting down")
         if hasattr(self, 'ser'):
-        self.ser.write(self.get_command(0,0))
-        self.ser.write(self.get_command(1,0))
-            self.ser.write(chr(0xA2))
-            self.ser.close()
+        self.ser.write(self.get_command(self._steering_servo_1,0))
+        self.ser.write(self.get_command(self._steering_servo_2,0))
+        self.ser.write(self.get_command(self._throttle_servo,0))
+        self.ser.write(chr(0xA2))
+        self.ser.close()
     
     def SteeringCallback(self,data):
-    ticks = self.DegToTicks(data.angle)
-        rospy.loginfo(rospy.get_name()+": Request Type="+str(data.request)+" Angle="+str(data.angle)+" Ticks="+str(ticks) )
-    ticks = self.DegToTicks(data.angle)
-        self.ser.write(self.get_command(0,ticks))
-        self.ser.write(self.get_command(1,ticks))
+        if data.request == SteeringCommand.Degrees:
+            ticks = self.DegToTicks(data.angle)
+            rospy.loginfo(rospy.get_name()+": Request Type="+str(data.request)+" Angle="+str(data.angle)+" Ticks="+str(ticks) )
+            ticks = self.DegToTicks(data.angle)
+            self.ser.write(self.get_command(self._steering_servo_1,ticks))
+            self.ser.write(self.get_command(self._steering_servo_2,ticks))
         
-
-        
+    def ThrottleCallback(selfself,data):
+        if data.request == ThrottleCommand.Relative:
+            ticks = data.position * 660
+            rospy.loginfo(rospy.get_name()+": position: "+str(data.position)+", ticks: "+str(ticks) )
+            self.ser.write(self.get_command(self._throttle_servo,ticks))
 
 
 #Init and run
